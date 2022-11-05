ea97a128f4107efdc4cb7f0298621d4fecb25b6f
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-11-05 15:10:02 +0200

    Init task3 HW-08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,21 @@
-//import { modulesaveToLocalStorage, loadFromLocalStorage } from './safe-storage-module';
+import { saveToLocalStorage, loadFromLocalStorage } from './safe-storage-module';
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const VIMEO_CURRENT_TIME_KEY = "videoplayer-current-time";
+let vimeoTimeOffset = loadFromLocalStorage(VIMEO_CURRENT_TIME_KEY) ?? 0;
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
+player.setCurrentTime(vimeoTimeOffset);
 
-player.on('play', function() {console.log('played the video!');});
+window.addEventListener("beforeunload", ()=>player.pause())
+
+const throttledTimeUpdateInStorege = throttle(UpdateTimeInStorage, 1000);
+
+player.on('timeupdate', throttledTimeUpdateInStorege);
+if (vimeoTimeOffset>0) player.play();
+
+function UpdateTimeInStorage({seconds}) {
+    saveToLocalStorage(VIMEO_CURRENT_TIME_KEY, seconds);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,20 @@
-import { modulesaveToLocalStorage, loadFromLocalStorage } from './safe-storage-module';
+import { saveToLocalStorage, loadFromLocalStorage } from './safe-storage-module';
+import throttle from 'lodash.throttle';
+
+const FORM_CONTENT_KEY = "feedback-form-state";
+let formContent = loadFromLocalStorage(FORM_CONTENT_KEY) ?? {
+    email: "",
+    msg: "",
+}
+
+const form = document.querySelector("form.feedback-form");
+debugger
+const throttledHandleForm = throttle(handleForm, 500);
+function handleForm(event) {
+    console.dir(event);
+};
+form.addEventListener("input", throttledHandleForm);
+
+
+
 
