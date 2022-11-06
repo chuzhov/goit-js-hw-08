e9a14781e93a743335224cc6f9f5165e32bdbfef
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-11-06 14:05:17 +0200

    Completes task 3 HW-08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,41 @@
-import { saveToLocalStorage, loadFromLocalStorage } from './safe-storage-module';
+import { saveToLocalStorage, loadFromLocalStorage, removeFromLocalStorage } from './safe-storage-module';
 import throttle from 'lodash.throttle';
 
 const FORM_CONTENT_KEY = "feedback-form-state";
 let formContent = loadFromLocalStorage(FORM_CONTENT_KEY) ?? {
     email: "",
-    msg: "",
+    message: "",
 }
 
 const form = document.querySelector("form.feedback-form");
-debugger
+
+if (formContent.email !=="" || formContent.message !=="") {
+    form.elements.email.value = formContent.email;
+    form.elements.message.value = formContent.message;
+};
+
 const throttledHandleForm = throttle(handleForm, 500);
 function handleForm(event) {
-    console.dir(event);
+    formContent[event.target.name] = event.target.value;
+    saveToLocalStorage(FORM_CONTENT_KEY, formContent);
 };
-form.addEventListener("input", throttledHandleForm);
 
+function submitForm(event) {
+    event.preventDefault();
+    // в заданні немає такого, але перевіряєм валідність даніх
+    if (formContent.email !=="" && formContent.message !=="") {
+        console.log(formContent);
+
+        removeFromLocalStorage(FORM_CONTENT_KEY);
+        formContent.email = "";
+        formContent.message = "";
+        
+        event.target.reset();
+    }
+}
+
+form.addEventListener("input", throttledHandleForm);
+form.addEventListener("submit", submitForm);
 
 
 
